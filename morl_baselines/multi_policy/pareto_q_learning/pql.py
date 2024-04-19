f72dfb921f97a522f5136c1b0a1f81222f7638be
chaymaa dkouk <chaymaadkouk674@gmail.com>
2024-04-19 11:42:37 +0200

    fix: reshape 3D ndarray to save as text

--- morl_baselines/multi_policy/pareto_q_learning/pql.py
+++ morl_baselines/multi_policy/pareto_q_learning/pql.py
@@ -381,9 +381,9 @@ class PQL(MOAgent):
             "epsilon_decay_steps": self.epsilon_decay_steps,
             "final_epsilon": self.final_epsilon,
             "ref_point": self.ref_point.tolist(),
-            "num_states": self.num_states,
-            "num_actions": self.num_actions,
-            "num_objectives": self.num_objectives
+            "num_states": float(self.num_states),
+            "num_actions": float(self.num_actions),
+            "num_objectives": float(self.num_objectives)
         }
         dump_json_file(path=params_path, data=pql_params)
 
